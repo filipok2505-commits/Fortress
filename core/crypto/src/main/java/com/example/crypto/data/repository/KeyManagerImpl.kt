6aefaaeb78207a2f716910eb796a75f70776ca40
Origami <filipok467@mail.ru>
2026-04-06 22:39:17 +0500

    core(crypto): add KeyManagerImpl

--- core/crypto/src/main/java/com/example/crypto/data/repository/KeyManagerImpl.kt
+++ core/crypto/src/main/java/com/example/crypto/data/repository/KeyManagerImpl.kt
@@ -1,18 +1,58 @@
 package com.example.crypto.data.repository
 
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import com.example.crypto.domain.repository.KeyManager
+import java.security.KeyStore
+import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 
 class KeyManagerImpl: KeyManager {
+
+    companion object {
+        private const val ANDROID_KEY_STORE = "AndroidKeyStore"
+        private const val KEY_ALGORITHM = "AES"
+        private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_GCM
+        private const val PADDING = KeyProperties.ENCRYPTION_PADDING_NONE
+        private const val KEY_SIZE = 256
+    }
+
+
     override fun getOrCreateKey(alias: String): SecretKey {
-        TODO("Not yet implemented")
+        val keyStore = KeyStore.getInstance(ANDROID_KEY_STORE).apply {
+            load(null)
+        }
+        keyStore.getKey(alias, null)?.let {
+            return it as SecretKey
+        }
+
+        val keyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM, ANDROID_KEY_STORE)
+
+        val keyGenParameterSpec = KeyGenParameterSpec.Builder(
+            alias,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+        )
+            .setBlockModes(BLOCK_MODE)
+            .setEncryptionPaddings(PADDING)
+            .setKeySize(KEY_SIZE)
+            .setUserAuthenticationRequired(false)
+            .build()
+
+        keyGenerator.init(keyGenParameterSpec)
+        return keyGenerator.generateKey()
     }
 
     override fun deleteKey(alias: String) {
-        TODO("Not yet implemented")
+        val keyStore = KeyStore.getInstance(ANDROID_KEY_STORE).apply {
+            load(null)
+        }
+        keyStore.deleteEntry(alias)
     }
 
     override fun containsKey(alias: String): Boolean {
-        TODO("Not yet implemented")
+        val keyStore = KeyStore.getInstance(ANDROID_KEY_STORE).apply {
+            load(null)
+        }
+        return keyStore.containsAlias(alias)
     }
 }
